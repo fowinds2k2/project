b2f6e6522ff58725ada10aab29eacf762c492d8e
zink <phucvinhlp02@gmail.com>
2022-11-10 09:57:39 +0700

    update home

--- js/home.js
+++ js/home.js
@@ -1,11 +1,14 @@
 jQuery(function($){
     var $navbar = $('.nav_bar');
+    var $gotop = $('.gotop');
     $(window).scroll(function(event){
         var $current = $(this).scrollTop();
-        if ($current > 0){
+        if ($current > 100){
             $navbar.addClass('navbar-color');
+            $gotop.show();
         } else {
             $navbar.removeClass('navbar-color');
+            $gotop.hide();
         }
     });
 });
