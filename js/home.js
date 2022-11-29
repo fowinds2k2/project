0c0219287a66e030a1970c4250f715feb2f4a54f
zink <phucvinhlp02@gmail.com>
2022-11-29 13:28:13 +0700

    update

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ jQuery(function($){
     var $navbar = $('.nav_bar');
     var $gotop = $('.gotop');
     var $belowct = $('.below_content');
+    $gotop.hide();
     $(window).scroll(function(event){
         var $current = $(this).scrollTop();
         if ($current == 0){
@@ -34,4 +35,19 @@ jQuery(function($){
             }
         }
     });
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 3000); // Change image every 2 seconds
+    }
 });
